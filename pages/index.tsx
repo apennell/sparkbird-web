ce7220468fb33530c250d882213e5eaed3bac022
Annie Pennell <anniecpennell@gmail.com>
2023-04-06 17:52:32 -0700

    Add minimal styling
    - Adds thistle (lavendar) and dark green brand colors and updates the variables using them
    - Adds some basic styling and semantic HTML updates
    - Adds favicon files using the sparkle icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,40 +16,58 @@ export default function Home() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>coming soon...</p>
-        </div>
-        <div className={styles.center}>
-          <h1>Sparkbird Works</h1>
-          <p>
-            Thoughtful and performant Web experiences powered by the principles
-            of{" "}
-            <a
-              href="https://www.w3.org/WAI/fundamentals/accessibility-intro/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              accessibility
-            </a>{" "}
-            and{" "}
-            <a
-              href="https://www.sustainablewebmanifesto.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              sustainability
-            </a>
-          </p>
-        </div>
+        <header>
+          <h1>
+            <span aria-hidden>₊⊹⁺˖</span> Sparkbird Works
+          </h1>
+        </header>
+
+        <p className={styles.headline}>
+          Thoughtful and performant Web experiences, powered by the principles
+          of{" "}
+          <a
+            href="https://www.w3.org/WAI/fundamentals/accessibility-intro/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            accessibility
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://www.sustainablewebmanifesto.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            sustainability
+          </a>
+        </p>
         <div>
-          <p>
+          <p className={styles.bodyLg}>
             Contact{" "}
             <a href="mailto:hello@sparkbird.works">hello@sparkbird.works</a> to
             inquire about our services.
           </p>
-          <p>Copyright Annie Pennell 2023</p>
+          <p className={styles.bodySm}>Copyright Annie Pennell 2023</p>
         </div>
       </main>
     </>
