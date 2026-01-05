c306515e4f5d1fbc2f95ed6e9543f32f1b9277be
Annie Pennell <anniecpennell@gmail.com>
2026-01-04 16:41:46 -0800

    Add resources to header nav

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,12 +1,18 @@
-import Link from "next/link";
+import Link from 'next/link';
 
-import styles from "./header.module.css";
+import styles from './header.module.css';
 
 export default function Header() {
   const navLinks = [
     {
-      _id: "contact",
-      href: "mailto:hello@sparkbird.works",
+      _id: 'resources',
+      href: 'resources',
+      title: 'Resources',
+      external: false,
+    },
+    {
+      _id: 'contact',
+      href: 'mailto:hello@sparkbird.works',
       title: (
         <>
           <span className={styles.desktopNavLink}>Get in touch</span>
@@ -18,25 +24,29 @@ export default function Header() {
   ];
 
   return (
-    <header>
-      <nav className={styles.nav}>
+    <header className={styles.headerContainer}>
+      <div className={styles.logoContainer}>
         <div className={styles.homeLink}>
           <Link href="/" className="titleLg">
             <span aria-hidden>₊⊹⁺˖</span> sparkbird works
           </Link>
         </div>
-        {navLinks.length > 0 &&
-          navLinks.map((link) => (
-            <div key={`${link._id}-nav`} className={styles.navItem}>
-              {link.external ? (
-                <a href={link.href} target="_blank" className="body1 monoLink">
-                  {link.title}
-                </a>
-              ) : (
-                <Link href={`/${link.href}`}>{link.title}</Link>
-              )}
+      </div>
+      <nav className={styles.linksContainer}>
+        {navLinks.map(({ href, external, title, _id }) => (
+          <div key={`${_id}-nav`} className={styles.linksGroup}>
+            <div className={styles.navItem}>
+              <a
+                href={href}
+                target={external ? '_blank' : undefined}
+                rel={external ? 'noopener noreferrer' : undefined}
+                className="body1"
+              >
+                {title}
+              </a>
             </div>
-          ))}
+          </div>
+        ))}
       </nav>
     </header>
   );
